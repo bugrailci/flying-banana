024c9ff5cbde927d9ad24a6b40f3c8ba09ebf8d4
kaissu <60655509+kaissu@users.noreply.github.com>
2020-04-13 22:41:04 +0300

    api kodlari
    hehe

---
+++ BynogameAPI/src/Bynogame.API/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BYNOGAME.API.Entities
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Sifre { get; set; }
+        public string Token { get; set; }
+    }
+}
